5005add0b7b6eb645b98007dabd4fd8f9704764c
Muhammad Hilman Imaduddin <hilmanimaduddin179@gmail.com>
2024-03-09 09:42:09 +0700

    third commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,74 @@
-"use client";
+import { Box, Button, Text } from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import CreatePage from "./indexthis";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 
-import FallingLetter from "@/component/motion/FallingLetter";
-import FromLeft from "@/component/motion/FromLeft";
-import FromNone from "@/component/motion/FromNone";
-import SetCountdown from "@/component/setCountDown";
-import SetDate from "@/component/setDate";
-import SetImage from "@/component/setImage";
-import SetTime from "@/component/setTime";
-import { Box, Text } from "@chakra-ui/react";
-import { useState } from "react";
-
-export default function CreatePage() {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleCreateData = () => {
-    setIsLoading(true);
+function AudioPlayer() {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [isShow, setIsShow] = useState(false);
+  const [isPlay, setIsPlay] = useState(true);
+  const handlePlayPause = () => {
+    if (audioRef.current) {
+      if (audioRef.current.paused) {
+        audioRef.current.play();
+        setIsPlay(true);
+      } else {
+        audioRef.current.pause();
+        setIsPlay(false);
+      }
+    }
   };
+
+  const OpenShow = () => {
+    setIsShow(true);
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
   return (
-    <Box display="flex" justifyContent="center" alignItems="center">
-      <Box
-        maxW="xl"
-        width="500px"
-        p={4}
-        display={"flex"}
-        flexDir={"column"}
-        gap={4}
-      >
-        <Box display="flex" justifyContent="space-between">
-          <FromLeft delay={1}>
-            <Box
-              bgColor={"#0a6c6c"}
-              color={"white"}
-              p={1}
-              paddingInline={2}
-              borderRadius={"md"}
-            >
-              <FallingLetter>
-                Assalamu alaikum, Sahabat Sesyurga...
-              </FallingLetter>
-            </Box>
-          </FromLeft>
-          <FromNone>
-            <SetTime />
-          </FromNone>
-        </Box>
-        <FromLeft delay={2}>
-          <SetDate />
-        </FromLeft>
-        <FromLeft delay={3}>
-          <SetImage />
-        </FromLeft>
-        <FromLeft delay={4}>
-          <SetCountdown />
-        </FromLeft>
-        <Box display={"flex"} justifyContent={"center"} p={4}>
-          <Text>By Extrarea</Text>
-        </Box>
+    <Box display={"flex"} flexDir={"column"}>
+      <Box display={"none"}>
+        <audio
+          src="/ramadhan-inst.mp3"
+          controls
+          style={{ width: "100%" }}
+          ref={audioRef}
+        />
       </Box>
+      {(isShow && (
+        <Box>
+          <CreatePage />
+          <Box
+            style={{ position: "fixed", bottom: 0, right: 0, zIndex: 100 }}
+            p={4}
+          >
+            <Button colorScheme="teal" onClick={handlePlayPause}>
+              <FontAwesomeIcon icon={isPlay ? faPause : faPlay} />
+            </Button>
+          </Box>
+        </Box>
+      )) || (
+        <Box
+          display={"flex"}
+          flexDir={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          h={"100vh"}
+          gap={4}
+        >
+          <Text textAlign={"center"}>Selamat datang, Wahai kawan</Text>
+          <Button colorScheme="teal" onClick={OpenShow}>
+            Mulai
+          </Button>
+          <Text style={{ position: "fixed", bottom: 20, zIndex: 100 }}>
+            By Extrarea
+          </Text>
+        </Box>
+      )}
     </Box>
   );
 }
+
+export default AudioPlayer;
---
+++ src/pages/indexthis.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import FallingLetter from "@/component/motion/FallingLetter";
+import FromLeft from "@/component/motion/FromLeft";
+import FromNone from "@/component/motion/FromNone";
+import SetCountdown from "@/component/setCountDown";
+import SetDate from "@/component/setDate";
+import SetImage from "@/component/setImage";
+import SetTime from "@/component/setTime";
+import { Box, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+export default function CreatePage() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleCreateData = () => {
+    setIsLoading(true);
+  };
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center">
+      <Box
+        maxW="xl"
+        width="500px"
+        p={4}
+        display={"flex"}
+        flexDir={"column"}
+        gap={4}
+      >
+        <Box display="flex" justifyContent="space-between">
+          <FromLeft delay={1}>
+            <Box
+              bgColor={"#0a6c6c"}
+              color={"white"}
+              p={1}
+              paddingInline={2}
+              borderRadius={"md"}
+            >
+              <FallingLetter>
+                Assalamu alaikum, Sahabat Sesyurga...
+              </FallingLetter>
+            </Box>
+          </FromLeft>
+          <FromNone>
+            <SetTime />
+          </FromNone>
+        </Box>
+        <FromLeft delay={2}>
+          <SetDate />
+        </FromLeft>
+        <FromLeft delay={3}>
+          <SetImage />
+        </FromLeft>
+        <FromLeft delay={4}>
+          <SetCountdown />
+        </FromLeft>
+        <Box display={"flex"} justifyContent={"center"} p={4}>
+          <Text>By Extrarea</Text>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
